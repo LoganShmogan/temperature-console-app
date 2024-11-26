f4e7b80c8baf96a57b23a0cfe50f05175cb294d2
Logan Young <younglogan09@gmail.com>
2024-11-26 21:12:12 +1300

    test run - little bit finiky will raise an issue

--- obj/Debug/net9.0/temperature-console-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/temperature-console-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temperature-console-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22eefc6ff655259c1eeecfc6c697be519c3e6384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106b0371cb349683bdea94018b284d7b692a45c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("temperature-console-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temperature-console-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
